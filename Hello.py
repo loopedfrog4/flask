c731e0c8374f405da5894bd8df58bbed7a7d15a1
loopedfrog4 <zhenghui39@hotmail.com>
2022-11-17 13:45:11 +0800

    Initialised MongoDB Connection

--- Hello.py
+++ Hello.py
@@ -1,5 +1,16 @@
 from flask import Flask, render_template, url_for, request
+from pymongo import MongoClient
+import pprint
+
 app = Flask(__name__)
+
+client = MongoClient('mongodb://localhost:27017/')
+db = client.userDB
+itemCollection = db.users
+
+for item in itemCollection.find({"username": "x@y.com"}):
+   pprint.pprint(item)
+
 
 @app.route('/', methods=['POST', 'GET'])
 def Hello():
