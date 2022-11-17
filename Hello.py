d8080d3f4f75cf33e55ef0f744a6a31bbfaacc63
loopedfrog4 <zhenghui39@hotmail.com>
2022-11-17 14:20:18 +0800

    Added pandas to read csv

--- Hello.py
+++ Hello.py
@@ -1,6 +1,11 @@
 from flask import Flask, render_template, url_for, request
 from pymongo import MongoClient
 import pprint
+import pandas as pd
+
+df = pd.read_csv('./data/listing-of-licensed-pharmacies-dec-2021-town.csv');
+
+print(df.to_string()) 
 
 app = Flask(__name__)
 
